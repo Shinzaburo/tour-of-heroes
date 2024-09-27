e881fd7cc1c9dd13e0cb1f440a41a1dacfbc3709
Shinzaburo <zaburo.0316@icloud.com>
2024-09-27 15:05:37 +0900

    新規作成画面の修正

--- src/app/hero-preview/hero-preview.component.ts
+++ src/app/hero-preview/hero-preview.component.ts
@@ -5,18 +5,16 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-preview',
   templateUrl: './hero-preview.component.html',
+  styleUrl: './hero-preview.component.css',
 })
 export class HeroPreviewComponent implements OnInit {
   hero: Hero | undefined;
-
-  showFlg = false;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.heroService.hero.subscribe((hero) => {
       this.hero = hero;
-      this.showFlg = this.hero !== undefined && this.hero.name !== '';
     });
   }
 }
